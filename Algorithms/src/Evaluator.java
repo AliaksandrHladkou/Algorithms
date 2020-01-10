d717f6d636e7a73b9a6969121f719190e1454694
Aliaksandr Hladkou <ahladkou@gmail.com>
2020-01-10 10:14:47 -0500

    LCM and GCD

--- Algorithms/src/Evaluator.java
+++ Algorithms/src/Evaluator.java
@@ -28,11 +28,45 @@ public class Evaluator {
         return largest;
     }
 
+    public int getGCD(int first, int second)
+    {
+        if (first < 0 || second < 0)
+            throw new ArithmeticException();
+        else if (first == 0 || second == 0)
+            return Math.abs(first-second);
+
+        while (first % second != 0)
+        {
+            int remainder = first % second;
+            first = second;
+            second = remainder;
+        }
+        return second;
+    }
+
+    public int getLCM(int first, int second)
+    {
+        if (first == 0 || second == 0)
+            return 0;
+        int max = Math.max(Math.abs(first), Math.abs(second));
+        int min = Math.min(Math.abs(first), Math.abs(second));
+        int lcm = max;
+
+        while (lcm % min != 0)
+        {
+            lcm += max;
+        }
+
+        return lcm;
+    }
+
     public static void main (String[] args)
     {
         int[] array = new int[]{4, 3, 5, 2, 10};
         Evaluator evaluator = new Evaluator();
         System.out.println(evaluator.getSmallest(array));
         System.out.println(evaluator.getLargest(array));
+        System.out.println("GCD is: " + evaluator.getGCD(array[1], array[4]));
+        System.out.println("LCM is: " + evaluator.getLCM(array[0], array[4]));
     }
 }
